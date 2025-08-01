fa7b24c999d5136af5e600fb6a2b9d7bc1474bd7
Agus Cahyono <cahyo.mamen@gmail.com>
2025-08-01 14:11:11 +0700

    initial files

---
+++ Sources/ACFieldModifiers/ACFieldModifiers.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+public struct ACFieldModifiers {
+    
+    public init() {
+    }
+    
+    public func greet() -> String {
+        return "Hello from ACFieldModifiers!"
+    }
+}
